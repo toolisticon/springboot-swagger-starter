b043faacdfdaa274cd75831fa8c4dce59ddfa98a
Jan Galinski <jan.galinski@holisticon.de>
2017-12-12 14:57:44 +0100

    supress logging, advance test

--- src/main/kotlin/Properties.kt
+++ src/main/kotlin/Properties.kt
@@ -2,12 +2,12 @@ package io.toolisticon.springboot.swagger
 
 import io.toolisticon.springboot.swagger.SwaggerProperties.Companion.PREFIX
 import lombok.Data
+import lombok.ToString
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 
 
 @ConfigurationProperties(PREFIX)
-@Data
 class SwaggerProperties {
 
     companion object {
@@ -20,14 +20,36 @@ class SwaggerProperties {
 
     var name = "default"
 
+    @NestedConfigurationProperty
+    var api: ApiInfoProperty? = ApiInfoProperty()
 
 
     @NestedConfigurationProperty
     var groups: List<SwaggerGroup> = mutableListOf()
 
+    override fun toString(): String {
+        return "SwaggerProperties(enabled=$enabled, redirect=$redirect, name='$name', api=$api, groups=$groups)"
+    }
+
+
 }
 
-@Data
 class SwaggerGroup {
     lateinit var name : String
+}
+
+class ApiInfoProperty {
+    var title : String? = null
+    var description: String? = null
+    var version: String? = null
+    var termsOfServiceUrl: String? = null
+    var contact: String? = null
+    var license: String? = null
+    var licenseUrl: String? = null
+
+    override fun toString(): String {
+        return "ApiInfoProperty(title='$title', description='$description', version='$version', termsOfServiceUrl='$termsOfServiceUrl', contact='$contact', license='$license', licenseUrl='$licenseUrl')"
+    }
+
+
 }
--- src/test/kotlin/SwaggerPropertiesTestHelper.kt
+++ src/test/kotlin/SwaggerPropertiesTestHelper.kt
@@ -1,15 +1,19 @@
 package io.toolisticon.springboot.swagger
 
+import mu.KLogging
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @EnableConfigurationProperties(SwaggerProperties::class)
 open abstract class SwaggerPropertiesTestHelper {
+
+    companion object : KLogging()
 
     @Autowired
     protected lateinit var properties: SwaggerProperties
@@ -26,9 +30,20 @@ class SwaggerPropertiesTest : SwaggerPropertiesTestHelper() {
     }
 }
 
+@TestPropertySource()
 class WithSingleGroupTest : SwaggerPropertiesTestHelper() {
     @Test
     fun `has one group in list`() {
         assertThat(properties.groups).hasSize(1)
+    }
+}
+
+@TestPropertySource("classpath:test/GlobalApiInfoPropertyTest.yml")
+class GlobalApiInfoPropertyTest : SwaggerPropertiesTestHelper() {
+
+
+    @Test
+    fun name() {
+logger.info { "${properties.api}" }
     }
 }
