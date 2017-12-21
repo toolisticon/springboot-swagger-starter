58538f75eef18181118955ebbcf730a82d565d1a
Jan Galinski <jan.galinski@holisticon.de>
2017-12-21 14:31:05 +0100

    introduce top-level apiInfo, fixes #8

--- src/test/kotlin/SwaggerPropertiesTest.kt
+++ src/test/kotlin/SwaggerPropertiesTest.kt
@@ -55,10 +55,86 @@ class WithSingleGroupTest : SwaggerPropertiesTestHelper() {
 
 
 @TestPropertySource(locations = ["classpath:test-toplevel-info.properties"])
-class GlobalApiInfoPropertyTest : SwaggerPropertiesTestHelper() {
+class RedirectPropertyTest : SwaggerPropertiesTestHelper() {
 
   @Test
   fun name() {
     assertThat(properties.redirect).isTrue()
+  }
+}
+
+@TestPropertySource(properties = [
+  "swagger.apiInfo.title=Some Title",
+  "swagger.apiInfo.description=Some Description",
+  "swagger.apiInfo.version=6.6.6",
+  "swagger.apiInfo.termsOfServiceUrl=http://terms.url",
+  "swagger.apiInfo.license=myLicense",
+  "swagger.apiInfo.licenseUrl=http://license.url",
+  "swagger.apiInfo.contact.name=Kermit",
+  "swagger.apiInfo.contact.url=http://muppets.url",
+  "swagger.apiInfo.contact.email=kermit@frog.the",
+  "swagger.dockets.foo.basePackage=foo"
+])
+class OverwriteDocketApiInfoTest : SwaggerPropertiesTestHelper() {
+
+  @Test
+  fun `apply global infos`() {
+    assertThat(properties.dockets).isNotEmpty
+    val foo = properties.dockets["foo"]!!.apiInfo
+
+    assertThat(foo.title).isEqualTo("Some Title")
+    assertThat(foo.description).isEqualTo("Some Description")
+    assertThat(foo.version).isEqualTo("6.6.6")
+    assertThat(foo.termsOfServiceUrl).isEqualTo("http://terms.url")
+    assertThat(foo.license).isEqualTo("myLicense")
+    assertThat(foo.licenseUrl).isEqualTo("http://license.url")
+    assertThat(foo.contact.name).isEqualTo("Kermit")
+    assertThat(foo.contact.url).isEqualTo("http://muppets.url")
+    assertThat(foo.contact.email).isEqualTo("kermit@frog.the")
+
+  }
+}
+
+
+@TestPropertySource(properties = [
+  "swagger.apiInfo.title=Some Title",
+  "swagger.apiInfo.description=Some Description",
+  "swagger.apiInfo.version=6.6.6",
+  "swagger.apiInfo.termsOfServiceUrl=http://terms.url",
+  "swagger.apiInfo.license=myLicense",
+  "swagger.apiInfo.licenseUrl=http://license.url",
+  "swagger.apiInfo.contact.name=Kermit",
+  "swagger.apiInfo.contact.url=http://muppets.url",
+  "swagger.apiInfo.contact.email=kermit@frog.the",
+  // docket: foo
+  "swagger.dockets.foo.basePackage=foo",
+  // overwrite global
+  "swagger.dockets.foo.apiInfo.title=Foo Title",
+  "swagger.dockets.foo.apiInfo.description=Foo Description",
+  "swagger.dockets.foo.apiInfo.version=6.6.7",
+  "swagger.dockets.foo.apiInfo.termsOfServiceUrl=http://foo-terms.url",
+  "swagger.dockets.foo.apiInfo.license=fooLicense",
+  "swagger.dockets.foo.apiInfo.licenseUrl=http://foo-license.url",
+  "swagger.dockets.foo.apiInfo.contact.name=Gonzo",
+  "swagger.dockets.foo.apiInfo.contact.url=http://foo-muppets.url",
+  "swagger.dockets.foo.apiInfo.contact.email=gonzo@great.the"
+])
+class DocketApiInfoOverwritesGlobalTest : SwaggerPropertiesTestHelper() {
+
+  @Test
+  fun `apply global infos`() {
+    assertThat(properties.dockets).isNotEmpty
+    val foo = properties.dockets["foo"]!!.apiInfo
+
+    assertThat(foo.title).isEqualTo("Foo Title")
+    assertThat(foo.description).isEqualTo("Foo Description")
+    assertThat(foo.version).isEqualTo("6.6.7")
+    assertThat(foo.termsOfServiceUrl).isEqualTo("http://foo-terms.url")
+    assertThat(foo.license).isEqualTo("fooLicense")
+    assertThat(foo.licenseUrl).isEqualTo("http://foo-license.url")
+    assertThat(foo.contact.name).isEqualTo("Gonzo")
+    assertThat(foo.contact.url).isEqualTo("http://foo-muppets.url")
+    assertThat(foo.contact.email).isEqualTo("gonzo@great.the")
+
   }
 }
