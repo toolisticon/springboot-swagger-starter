9bf2156121c1d38eb3dc2a277aefd8d7c2cb4e5b
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-07-13 12:55:19 +0200

    switch to immutable data classes for properties

--- src/main/kotlin/Properties.kt
+++ src/main/kotlin/Properties.kt
@@ -1,6 +1,7 @@
 package io.toolisticon.springboot.swagger
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.ApiInfo.DEFAULT
@@ -10,35 +11,41 @@ import java.util.function.Supplier
 import javax.annotation.PostConstruct
 
 @ConfigurationProperties("swagger")
+@ConstructorBinding
 data class SwaggerProperties(
-  var enabled: Boolean = true,
-  var redirect: Boolean = false,
-
+  val enabled: Boolean = true,
+  val redirect: Boolean = false,
   @NestedConfigurationProperty
-  var apiInfo: ApiInfoProperty = ApiInfoProperty(),
-
+  val apiInfo: ApiInfoProperty = ApiInfoProperty(),
   @NestedConfigurationProperty
   var dockets: Map<String, DocketProperty> = mutableMapOf()
 ) {
 
-  private fun replaceWithSuper(docketValue: String, globalValue: String, defaultValue: String): String =
-    if (docketValue == defaultValue) globalValue else docketValue
-
   @PostConstruct
-  fun init() = dockets.map { it.value.apiInfo }
-    .map {
-      with(it) {
-        title = replaceWithSuper(title, apiInfo.title, DEFAULT.title)
-        description = replaceWithSuper(description, apiInfo.description, DEFAULT.description)
-        version = replaceWithSuper(version, apiInfo.version, DEFAULT.version)
-        termsOfServiceUrl = replaceWithSuper(termsOfServiceUrl, apiInfo.termsOfServiceUrl, DEFAULT.termsOfServiceUrl)
-        license = replaceWithSuper(license, apiInfo.license, DEFAULT.license)
-        licenseUrl = replaceWithSuper(licenseUrl, apiInfo.licenseUrl, DEFAULT.licenseUrl)
-        contact.name = replaceWithSuper(contact.name, apiInfo.contact.name, DEFAULT_CONTACT.name)
-        contact.url = replaceWithSuper(contact.url, apiInfo.contact.url, DEFAULT_CONTACT.url)
-        contact.email = replaceWithSuper(contact.email, apiInfo.contact.email, DEFAULT_CONTACT.email)
-      }
+  fun processDefaults() {
+    this.dockets = processDefaults(dockets, this.apiInfo)
+  }
+
+  companion object {
+    private fun processDefaults(dockets: Map<String, DocketProperty>, apiInfo: ApiInfoProperty) = dockets.mapValues {
+      it.value.copy(apiInfo = ApiInfoProperty(
+        title = replaceWithSuper(it.value.apiInfo.title, apiInfo.title, DEFAULT.title),
+        description = replaceWithSuper(it.value.apiInfo.description, apiInfo.description, DEFAULT.description),
+        version = replaceWithSuper(it.value.apiInfo.version, apiInfo.version, DEFAULT.version),
+        termsOfServiceUrl = replaceWithSuper(it.value.apiInfo.termsOfServiceUrl, apiInfo.termsOfServiceUrl, DEFAULT.termsOfServiceUrl),
+        license = replaceWithSuper(it.value.apiInfo.license, apiInfo.license, DEFAULT.license),
+        licenseUrl = replaceWithSuper(it.value.apiInfo.licenseUrl, apiInfo.licenseUrl, DEFAULT.licenseUrl),
+        contact = it.value.apiInfo.contact.copy(
+          name = replaceWithSuper(it.value.apiInfo.contact.name, apiInfo.contact.name, DEFAULT_CONTACT.name),
+          url = replaceWithSuper(it.value.apiInfo.contact.url, apiInfo.contact.url, DEFAULT_CONTACT.url),
+          email = replaceWithSuper(it.value.apiInfo.contact.email, apiInfo.contact.email, DEFAULT_CONTACT.email)
+        )
+      ))
     }
+
+    private fun replaceWithSuper(docketValue: String, globalValue: String, defaultValue: String): String =
+      if (docketValue == defaultValue) globalValue else docketValue
+  }
 }
 
 data class DocketProperty(
@@ -48,25 +55,27 @@ data class DocketProperty(
   var path: String = ""
 )
 
+@ConstructorBinding
 data class ApiInfoProperty(
-  var title: String = DEFAULT.title,
-  var description: String = DEFAULT.description,
-  var version: String = DEFAULT.version,
-  var termsOfServiceUrl: String = DEFAULT.termsOfServiceUrl,
-  var license: String = DEFAULT.license,
-  var licenseUrl: String = DEFAULT.licenseUrl,
+  val title: String = DEFAULT.title,
+  val description: String = DEFAULT.description,
+  val version: String = DEFAULT.version,
+  val termsOfServiceUrl: String = DEFAULT.termsOfServiceUrl,
+  val license: String = DEFAULT.license,
+  val licenseUrl: String = DEFAULT.licenseUrl,
 
   @NestedConfigurationProperty
-  var contact: ContactProperty = ContactProperty()
+  val contact: ContactProperty = ContactProperty()
 ) : Supplier<ApiInfo> {
 
   override fun get(): ApiInfo = ApiInfo(title, description, version, termsOfServiceUrl, contact.get(), license, licenseUrl, mutableListOf())
 }
 
+@ConstructorBinding
 data class ContactProperty(
-  var name: String = DEFAULT_CONTACT.name,
-  var url: String = DEFAULT_CONTACT.url,
-  var email: String = DEFAULT_CONTACT.email
+  val name: String = DEFAULT_CONTACT.name,
+  val url: String = DEFAULT_CONTACT.url,
+  val email: String = DEFAULT_CONTACT.email
 ) : Supplier<Contact> {
 
   override fun get(): Contact = Contact(name, url, email)
